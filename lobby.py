2d31c3b5c2198ba90d664638860146a8a721b0f5
eunsol-k <solsol1203@gmail.com>
2023-04-18 12:05:13 +0900

    실행 불가 오류 수정

--- lobby.py
+++ lobby.py
@@ -2,19 +2,16 @@ import pygame
 import setting
 import time
 
-<<<<<<< HEAD
 # 상수 정의
 EMPTY_COLOR = (255, 255, 255)
 PLAYER_COLOR = (255, 255, 0)
 COMPUTER_COLOR = (100,100,100)
-=======
 
 
 # 상수 정의
 EMPTY_COLOR = (255, 255, 255)
 PLAYER_COLOR = (255, 255, 0)
-COMPUTER_COLOR = (100,100,100) 
->>>>>>> Jisu
+COMPUTER_COLOR = (100,100,100)
 GAMESTART_COLOR = (0, 255, 0)
 BACK_COLOR = (0, 0, 255)
 
@@ -26,11 +23,7 @@ def input_text_on_surface(screen, font, surface_rect):
                 pygame.quit()
                 quit()
             elif event.type == pygame.KEYDOWN:
-<<<<<<< HEAD
                 if event.key == setting.get_keymap_check(): # pygame.K_RETURN
-=======
-                if event.key == pygame.K_RETURN:
->>>>>>> Jisu
                     return text
                 elif event.key == pygame.K_BACKSPACE:
                     text = text[:-1]
