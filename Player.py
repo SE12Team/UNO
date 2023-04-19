396d5665bca54bfb717a520ba67b51055fc7e6b8
Jisu0528 <71203375+Jisu0528@users.noreply.github.com>
2023-04-20 02:54:55 +0900

    add Player

--- Player.py
+++ Player.py
@@ -1,40 +1,61 @@
-import random
-import time 
-
-class PlayerClass:
-    def __init__(self, name, hand):
+class Player:
+    def __init__(self, name):
         self.name = name
-        self.hand = hand
-
-    def __repr__(self):
-        return self.name
+        self.hand = []
 
     def getHand(self):
         return self.hand
-
-    def addCardToHand(self, card):
+    
+    '''
+    덱에서 카드를 뽑아 player 손에 추가
+    Parameter: deck -> list
+    Return: None
+    '''
+    def addCard(self, deck):
+        card = deck.drawCard() # deck에서 pop한 것과 연결
         self.hand.append(card)
 
-    def removeCardFromHand(self, card):
-        self.hand.remove(card)
-
-    def hasCard(self, colors, value):
+    '''
+    player가 카드를 낼 수 있는지 확인
+    Parameter: color -> string, value -> string
+    Return: boolean
+    '''
+    def canPlay(self, color, value):
         for card in self.hand:
-            if "Wild" in card.color: 
+            if "Wild" in card:
                 return True
-            elif colors in card.color or value in card.value:
+            elif color in card or value in card:
                 return True
         return False
     
-    def select_card(self, card):
-        current_player = self.players[self.current_player_index]
-        current_player.removeCard(card)
-        self.board.play_card(card)
-        self.current_player_index = (self.current_player_index + 1) % len(self.players)
-        
-    def uno(self):
-        if len(self.hand) == 1:
-            time.sleep(random.uniform(0.5, 1)) # 0.5초에서 1초 사이로 무작위로 대기
+    '''
+    player가 카드를 플레이
+    Parameter: card_index, discard_pile, color, value
+    Return: boolean
+    '''
+    def playCard(self, card_index, discard_pile, color, value):
+        card = self.hand[card_index]
+        if self.canPlay(color, value):
+            self.hand.pop(card_index) # 손에서 카드 제거
+            discard_pile.append(card) # 카드를 카드 더미에 추가
             return True
         else:
+            print("선택한 카드를 플레이할 수 없음")
             return False
+
+    '''
+    player 손의 카드 리스트 보여주기
+    Parameter: self.hand -> list
+    Return: None
+    '''
+    def showHand(self):
+        for card in self.hand:
+            print(card)
+        print("")
+    
+    '''
+    player가 승리했는지 여부를 반환
+    Return: boolean
+    '''
+    def hasWon(self):
+        return len(self.hand) == 0
